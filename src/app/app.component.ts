2908d07079977f07d41d461c7b854bb696cbb235
Abdou aziz <abdouaziz0104@gmail.com>
2025-08-19 21:24:19 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  standalone: true,
+  styleUrls: ['./dashboard.component.scss']
 })
-export class AppComponent {
-  title = 'AppAxiomeGroupBTP-FRONT';
+export class DashboardComponent implements OnInit {
+
+  stats = {
+    totalChantiers: 24,
+    chantiersActifs: 8,
+    chantiersTermines: 12,
+    budgetTotal: 1250000,
+    incidentsOuverts: 3
+  };
+
+  recentActivities = [
+    { action: 'Nouveau chantier créé', user: 'Jean Dupont', time: '2 min ago' },
+    { action: 'Document uploadé', user: 'Marie Martin', time: '5 min ago' },
+    { action: 'Incident résolu', user: 'Pierre Durand', time: '10 min ago' }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  getProgressValue(completed: number, total: number): number {
+    return total > 0 ? (completed / total) * 100 : 0;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,57 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// Angular Material Modules
+// Angular Material Modules
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, DashboardComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    // Material
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+   
+
   ],
   providers: [],
   bootstrap: [AppComponent]
